b53880035e2dad19a211153c57fa27a1c7057581
srdjanjevtic <srdjanjevticle@gmail.com>
2023-10-11 16:34:12 +0200

    mongodb added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3330;
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.xpltb.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
 
 app.use(express.json());
 app.use(cors());
