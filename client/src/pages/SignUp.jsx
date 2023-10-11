882566f1062ec68d2b76bb09e15135f93647d46d
srdjanjevtic <srdjanjevticle@gmail.com>
2023-10-11 20:28:21 +0200

    sign-up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-xl"
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-xl"
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-xl"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-xl uppercase hover:opacity-75 disabled:opacity-30">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/">
+          <span className="text-blue-700">Log In</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoutes.js";
+import { errHandlerG } from "./middleware/errorHandlerGlobal.js";
 
 const app = express();
 const port = process.env.PORT || 3330;
@@ -23,13 +24,13 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRouter);
+app.use("/api/auth", errHandlerG, authRouter);
 app.use("/api/test", userRouter);
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({ success: false, statusCode, message });
-});
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   return res.status(statusCode).json({ success: false, statusCode, message });
+// });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
