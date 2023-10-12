97b4a77b969dba291cbffa878be14f98be176528
srdjanjevtic <srdjanjevticle@gmail.com>
2023-10-12 14:23:08 +0200

    login route completed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 import User from "../models/UserModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -12,6 +16,30 @@ export const signup = async (req, res, next) => {
     return res
       .status(201)
       .json({ message: `User ${username} created successfully` });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({ message: `User ${validUser.username} is logged in` });
   } catch (error) {
     next(error);
   }
