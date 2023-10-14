7a1cc3e2d0df54ba543160cdcb62c67f05f3ea9f
srdjanjevtic <srdjanjevticle@gmail.com>
2023-10-14 05:38:57 +0200

    private

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,36 +1,103 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { firebaseApp } from "../firebase";
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [uploadError, setUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(firebaseApp);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on("state_changed", (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setUploadPercentage(Math.round(progress));
+      (uploadError) => {
+        setUploadError(true);
+        console.log(uploadError);
+      };
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      };
+    });
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-2 border-double border-0  border-slate-300 px-6 py-4 rounded-xl shadow-slate-700 shadow-md">
+        <input
+          onChange={(event) => setFile(event.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          onClick={() => fileRef.current.click()}
         />
+        <p className="text-sm self-center">
+          {uploadError ? (
+            <span className="text-red-700">
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : uploadPercentage > 0 && uploadPercentage < 100 ? (
+            <span className="text-slate-700">{`Uploading ${uploadPercentage}%`}</span>
+          ) : uploadPercentage === 100 ? (
+            <span className="text-green-700">Image successfully uploaded!</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           id="username"
           type="text"
           placeholder="username"
           className="border p-2 rounded-xl mt-3"
+          onChange={handleChange}
         />
         <input
           id="email"
           type="email"
           placeholder="email"
           className="border p-2 rounded-xl mt-3"
+          onChange={handleChange}
         />
         <input
           id="password"
-          type="password"
+          type="text"
           placeholder="password"
           className="border p-2 rounded-xl mt-3"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 rounded-md text-white p-2 uppercase hover:opacity-75 disabled:opacity-30">
+        <button className="bg-slate-700 rounded-md text-white p-2 mt-3 uppercase hover:opacity-75 disabled:opacity-30">
           Update
         </button>
       </form>
